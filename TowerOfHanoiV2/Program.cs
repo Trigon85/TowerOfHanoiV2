eeb0ce39a15c991bdd0157d96689d0e9fe69a0a2
Adam Elswick <S01239774@acad.tri-c.edu>
2021-03-02 12:48:06 -0500

    Add project files.

---
+++ TowerOfHanoiV2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class TowerOfHanoi
+{
+   static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+   {
+      if (n == 1)
+      {
+         Console.WriteLine("Move disk 1 from rod " +  
+                           from_rod + " to rod " + to_rod);
+         return;
+      }
+      towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+      Console.WriteLine("Move disk " + n + " from rod " +  
+                        from_rod + " to rod " + to_rod);
+      towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+   }
+     
+  
+   public static void Main(String []args)
+   {
+      int n = 3;
+      towerOfHanoi(n, 'A', 'C', 'B');
+   }
+}
+ 
